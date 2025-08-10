39254e3010a498ee3598d78b5d64ffb06c42397c
Eason <eason@elvtd.io>
2025-08-10 16:29:23 +0800

    chore: Update deployment config for netlify adding trailing slash

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/wayfinder",
   reactStrictMode: true,
+  trailingSlash: true,
   output: "standalone",
   images: {
     unoptimized: true,
